2002b25831cea4b0dac1ad63f65c101b2bd5e448
MustafaZemin <76277936+MustafaZemin@users.noreply.github.com>
2024-05-12 16:16:50 +0300

    Update CreateCampaign.jsx

--- src/pages/CreateCampaign.jsx
+++ src/pages/CreateCampaign.jsx
@@ -15,7 +15,7 @@ const CreateCampaign = () => {
     proposal: '',
   });
 
-  const youtubeUrlRegex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/(?!shorts\/).+$/;
+  const youtubeUrlRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/|youtu\.be\/).+$/i;
 
   const handleFormFieldChange = (fieldName, e) => {
     setForm({ ...form, [fieldName]: e.target.value });
